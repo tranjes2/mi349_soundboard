fbacb69601c0b4a6027f6df5ddc49674d422281c
Jessica Tran <tranjes2@msu.edu>
2021-11-18 16:52:00 -0500

    creating soundboard with html, JS, CSS, images, audio files

--- soundboard.js
+++ soundboard.js
@@ -1,1 +1,30 @@
+let joeyAudio = document.getElementById('joey-audio')
+let monicaAudio = document.getElementById('monica-audio')
+let chandlerAudio = document.getElementById('chandler-audio')
+let phoebeAudio = document.getElementById('phoebe-audio')
+let rossAudio = document.getElementById('ross-audio')
+let rachelAudio = document.getElementById('rachel-audio')
 
+document.getElementById('joey').onclick = function() {
+    joeyAudio.play();
+}
+
+document.getElementById('monica').onclick = function() {
+    monicaAudio.play();
+}
+
+document.getElementById('chandler').onclick = function() {
+    chandlerAudio.play();
+}
+
+document.getElementById('phoebe').onclick = function() {
+    phoebeAudio.play();
+}
+
+document.getElementById('ross').onclick = function() {
+    rossAudio.play();
+}
+
+document.getElementById('rachel').onclick = function() {
+    rachelAudio.play();
+}
